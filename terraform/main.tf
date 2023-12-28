e09b3961c8b4fc8d97ad625c9d0ee0276373af21
pavel-petrov-ua <88945003+pavel-petrov-ua@users.noreply.github.com>
2023-12-28 18:40:56 +0200

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ terraform {
     bucket         = "petrov-pavlo-2023-11-20"
     key            = "aws/main/terraform.tfstate"
     region         = "us-east-1"  # Замініть на свою регіон AWS
-    encrypt        = truete
+    encrypt        = true
   }
 }
 
