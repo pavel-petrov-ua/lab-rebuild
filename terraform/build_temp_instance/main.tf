f7207ed7415d26fca68ddb4d44063a6615f84750
pavel-petrov-ua <88945003+pavel-petrov-ua@users.noreply.github.com>
2023-12-30 14:42:14 +0200

    hz

--- terraform/build_temp_instance/main.tf
+++ terraform/build_temp_instance/main.tf
@@ -30,24 +30,24 @@ resource "aws_instance" "ami_ring_ring" {
   instance_type = "t2.micro"
   subnet_id     = data.terraform_remote_state.main_line.outputs.subnet_ids[1]
   key_name      = "EC2_GITHUB"
-#  vpc_security_group_ids = [data.terraform_remote_state.main_line.outputs.security_group_id]
-  security_group = [data.terraform_remote_state.main_line.outputs.security_group_id]
-
+  vpc_security_group_ids = [data.terraform_remote_state.main_line.outputs.security_group_id]
+  associate_public_ip_address = true
+ 
   tags = {
     Name = "example-instance"
   }
-  provisioner "remote-exec" {
-  inline = ["echo 'Wait until SSH is ready'"]
+#   provisioner "remote-exec" {
+#   inline = ["echo 'Wait until SSH is ready'"]
 
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = base64decode("${var.TF_VAR_secret_key}")
-#     private_key = file"${TF_VAR_secret_key}"
-#     private_key = data.github_actions_secret.private_key.name
-      host        = aws_instance.ami_ring_ring.public_ip
-    }
-  }
+#     connection {
+#       type        = "ssh"
+#       user        = "ubuntu"
+#       private_key = base64decode("${var.TF_VAR_secret_key}")
+# #     private_key = file"${TF_VAR_secret_key}"
+# #     private_key = data.github_actions_secret.private_key.name
+#       host        = aws_instance.ami_ring_ring.public_ip
+#     }
+#   }
 
 #  provisioner "local-exec" {
 #    command = "ansible-playbook  -i ${aws_instance.nginx.public_ip}, --private-key ${local.private_key_path} nginx.yaml"
