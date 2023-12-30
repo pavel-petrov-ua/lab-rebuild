38c4eedd2f5ea77ceed443aae318982264dfe5a0
pavel-petrov-ua <88945003+pavel-petrov-ua@users.noreply.github.com>
2023-12-30 15:13:47 +0200

    hz

--- terraform/vpc-outputs.tf
+++ terraform/vpc-outputs.tf
@@ -7,10 +7,11 @@ output "subnet_ids" {
   value = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id, aws_subnet.subnet_c.id]
 }
 
-output "aws_igw_id" {
+
+output "internet_gateway_id" {
   value = aws_internet_gateway.ring_ring_igw.id
 }
 
-output "road_table_id" {
+output "route_table_id" {
   value = aws_route_table.example_route_table.id
 }
