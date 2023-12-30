eeec89f1edb92a90b55c67a1a073f814f9d289d2
pavel-petrov-ua <88945003+pavel-petrov-ua@users.noreply.github.com>
2023-12-30 12:47:13 +0200

    hmmmm

---
+++ terraform/build_temp_instance/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-east-1"  # Замініть на свою регіон AWS
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "petrov-pavlo-2023-11-20"
+    key            = "aws/main/build_temp_instance/terraform.tfstate"
+    region         = "us-east-1"  
+    encrypt        = true
+  }
+}
+
+data "terraform_remote_state" "other_project" {
+  backend = "s3"
+  config = {
+    bucket = "petrov-pavlo-2023-11-20"
+    key    = "aws/main/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "ami_ring_ring" {
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+  subnet_id     = [data.terraform_remote_state.main_line.outputs.subnet_ids]
+
+  vpc_security_group_ids = [data.terraform_remote_state.main_line.outputs.security_group_id]
+
+  tags = {
+    Name = "example-instance"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.example.public_ip
+}
